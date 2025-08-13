05114dc8d485d8caf43f67abe35167bfdcaa1d5a
javi <javimusama@gmail.com>
2025-08-13 17:47:27 +0200

    forgot #endif

--- tests/Core/main.c
+++ tests/Core/main.c
@@ -131,8 +131,7 @@ void Error_Handler(void)
   */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-
   /* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-
 }
+#endif /* USE_FULL_ASSERT */
