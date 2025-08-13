2723537d5a1b7a1aed3267c7a111528eb4507f80
javi <javimusama@gmail.com>
2025-08-13 17:40:12 +0200

    forgot to add function definitions

--- tests/unity_uart_output.c
+++ tests/unity_uart_output.c
@@ -1,10 +1,9 @@
 // unity_uart_output.c
 #include "unity_uart_output.h"
-
-extern UART_HandleTypeDef huart2; // or your chosen UART
+#include "usart.h"
 
 void unity_uart_putc(int c)
 {
     uint8_t ch = (uint8_t)c;
-    HAL_UART_Transmit(&huart2, &ch, 1, HAL_MAX_DELAY);
+    HAL_UART_Transmit(&lpuart1, &ch, 1, HAL_MAX_DELAY);
 }
