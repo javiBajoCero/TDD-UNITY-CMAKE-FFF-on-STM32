9ed857275d10f202b370b7c075508f04a3042a31
javi <javimusama@gmail.com>
2025-08-14 12:28:50 +0200

    failing test

--- tests/Core/test_wolksvagen.c
+++ tests/Core/test_wolksvagen.c
@@ -16,7 +16,12 @@ TEST(Wolksvagen, test_always_passes_2) {
     TEST_ASSERT_TRUE(1);
 }
 
+TEST(Wolksvagen, test_fails) {
+    TEST_ASSERT_TRUE(0);
+}
+
 TEST_GROUP_RUNNER(Wolksvagen) {
     RUN_TEST_CASE(Wolksvagen, test_always_passes);
     RUN_TEST_CASE(Wolksvagen, test_always_passes_2);
+    RUN_TEST_CASE(Wolksvagen, test_fails);
 }
